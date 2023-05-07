b1b87168bf1bc25a13e0567558ac54bb8118f91c
LvnL <levin.leesemann@gmail.com>
2023-05-07 18:03:15 -0500

    Add COO row and column indicies for GPU

--- src/runner.cpp
+++ src/runner.cpp
@@ -8,8 +8,8 @@
 
 using namespace std;
 
-int numberOfRows, numberOfColumns;
-vector<int> rowIndices, columnIndices, updatedVertex, updatedVertexIndices;
+int rowCount, columnCount;
+vector<int> cpuRows, cpuColumns, gpuRows, gpuColumns, updatedVertex, updatedVertexIndices;
 vector<float> B, C, values;
 
 bool compareRows(const vector<int>& vectorA, const vector<int>& vectorB) {
@@ -18,45 +18,47 @@ bool compareRows(const vector<int>& vectorA, const vector<int>& vectorB) {
 
 int main(int argc, char* argv[]) {
 	ifstream inputFile(argv[1]);
-    vector<vector<int>> rowColumnIndices;
+    vector<vector<int>> cooEdges;
 
 	cout << "Reading file \"" << argv[1] << "\"... " << flush;
 
     int row, column;
 	while (inputFile >> row >> column) {
 		vector<int> rowColumnIndex{row - 1, column - 1};
-		rowColumnIndices.push_back(rowColumnIndex);
+        gpuRows.push_back(row - 1);
+        gpuColumns.push_back(column - 1);
+		cooEdges.push_back(rowColumnIndex);
         values.push_back(1);
 
-		if (row > numberOfRows) numberOfRows = row;
-        if (column > numberOfColumns) numberOfColumns = column;
+		if (row > rowCount) rowCount = row;
+        if (column > columnCount) columnCount = column;
     }	
 
     cout << "done" << endl;
 
     cout << "Sorting edges by row... " << flush;
 
-	sort(rowColumnIndices.begin(), rowColumnIndices.end(), compareRows);
+	sort(cooEdges.begin(), cooEdges.end(), compareRows);
 
     cout << "done" << endl;
 
     cout << "Storing edges as CSR... " << flush;
     
-	rowIndices.push_back(0);
-	columnIndices.push_back(rowColumnIndices[0][1]);
+	cpuRows.push_back(0);
+	cpuColumns.push_back(cooEdges[0][1]);
     
     int offset = 0;
 	for (int i = 1; i < values.size(); i++) {
-		if (rowColumnIndices[i][0] != rowColumnIndices[i - 1][0]) {
-			for (int j = 0; j < rowColumnIndices[i][0] - rowColumnIndices[i - 1][0]; j++)
-				rowIndices.push_back(rowIndices.back());
+		if (cooEdges[i][0] != cooEdges[i - 1][0]) {
+			for (int j = 0; j < cooEdges[i][0] - cooEdges[i - 1][0]; j++)
+				cpuRows.push_back(cpuRows.back());
 
-			if (rowColumnIndices[i][0] == 1) offset += 1;
+			if (cooEdges[i][0] == 1) offset += 1;
 
-			rowIndices.back() = rowIndices.back() + offset;
+			cpuRows.back() = cpuRows.back() + offset;
 			offset = 0;
 		}
-		columnIndices.push_back(rowColumnIndices[i][1]);
+		cpuColumns.push_back(cooEdges[i][1]);
 		offset++;
 	}
 
@@ -64,20 +66,20 @@ int main(int argc, char* argv[]) {
     
     cout << "Initializing paths... " << flush;
 
-    for (int i = 0; i < numberOfColumns; i++)
+    for (int i = 0; i < columnCount; i++)
         B.push_back(9999999);
-    for (int i = 0; i < numberOfRows; i++)
+    for (int i = 0; i < rowCount; i++)
         C.push_back(9999999);
     
     cout << "done" << endl;
 
     B[0] = 0;
-    updatedVertex.resize(numberOfColumns, 0);
+    updatedVertex.resize(columnCount, 0);
     updatedVertex[0] = 1;
     updatedVertexIndices.push_back(0);
     
     // Iterate Bellman-Ford
-    for (int i = 0; i < numberOfRows; i++) {
+    for (int i = 0; i < rowCount; i++) {
         cout << "Iteration: " << i << endl;
         cout << "    Vertices being processed: " << updatedVertexIndices.size() / (float) B.size() * 100 << "%" << endl;
 
@@ -87,7 +89,7 @@ int main(int argc, char* argv[]) {
             for (int i = 0; i < updatedVertex.size(); i++)
                 updatedVertex[i] = 0;
 
-            runGPU(B, C, values, rowIndices, columnIndices, numberOfRows, updatedVertex);
+            runGPU(B, C, values, gpuRows, gpuColumns, rowCount, updatedVertex);
 
             // Synchronize updatedVertexIndices to match updatedVertex
             updatedVertexIndices.clear();
@@ -96,7 +98,7 @@ int main(int argc, char* argv[]) {
         } else {
             cout << "    Starting CPU iteration... " << flush;
 
-            updatedVertexIndices = runCPU(B, C, values, rowIndices, columnIndices, updatedVertexIndices, numberOfRows, 1);
+            updatedVertexIndices = runCPU(B, C, values, cpuRows, cpuColumns, updatedVertexIndices, rowCount, 1);
         }
 
         cout << "done" << endl;
